c1ee0f6699a310c598f085c7adccb57d759046a2
cameronly08 <cameron.ly@education.nsw.gov.au>
2023-10-25 15:02:37 +1100

    commit gesture sensor save

--- UltraGlove/cl.functions.ino
+++ UltraGlove/cl.functions.ino
@@ -1,25 +1,22 @@
+/*
+The following simple demo will show you a very easy application: When you move up, the red led will be turned on, otherwise the red led will be turned off.
+*/
 #include <Wire.h>
 #include "paj7620.h"
 
-#define LED_PIN 4
-#define GES_UP_FLAG 1
-#define GES_DOWN_FLAG 2
-
 void setup()
 {
-  Wire.begin();
-  pinMode(LED_PIN, OUTPUT);
-  paj7620Init(); // Assuming the function is provided by the library and properly set up
+  paj7620Init();
 }
 
 void loop()
 {
-  uint8_t data = 0;  // Read Bank_0_Reg_0x43/0x44 for gesture result.
+ uint8_t data = 0;  // Read Bank_0_Reg_0x43/0x44 for gesture result.
 
-  paj7620ReadReg(0x43, 1, &data);  // When different gestures are detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
+ paj7620ReadReg(0x43, 1, &data);  // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
 
-  if (data == GES_UP_FLAG)      // When up gesture is detected, the variable 'data' will be set to GES_UP_FLAG.
-    digitalWrite(LED_PIN, HIGH);      // turn the LED on (HIGH is the voltage level)
-  else if (data == GES_DOWN_FLAG)      // When down gesture is detected, the variable 'data' will be set to GES_DOWN_FLAG.
-    digitalWrite(LED_PIN, LOW);       // turn the LED off by making the voltage LOW
+ if (data == GES_UP_FLAG)      // When up gesture be detected,the variable 'data' will be set to GES_UP_FLAG.
+  digitalWrite(4, HIGH);      // turn the LED on (HIGH is the voltage level)
+ if (data == GES_DOWN_FLAG)      // When down gesture be detected,the variable 'data' will be set to GES_DOWN_FLAG.
+        digitalWrite(4, LOW);       // turn the LED off by making the voltage LOW
 }
